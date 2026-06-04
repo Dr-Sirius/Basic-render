c6facf3179ff10d710f710601b3848e7f7ebe9ff
Ayden McNew <mcnew1721@gmail.com>
2026-06-04 15:52:24 -0400

    added face rendering | currently not running with noise params

--- src/Block.h
+++ src/Block.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <print>
 #include <raylib.h>
+#include <raymath.h>
 #include <vector>
 
 #include "Util.h"
@@ -17,10 +18,32 @@ enum BLOCK_TYPE
   GROUND,
 };
 
+struct Face
+{
+  Model model;
+  Vector3 position;
+  Vector3 rotation;
+  float angle;
+  bool render;
+  void
+  DrawFace ()
+  {
+    if (render)
+      DrawModelEx (model, position, rotation, angle, Vector3One (), WHITE);
+  }
+};
+
 struct Block
 {
   // Mesh block;
-  Model model;
+  // Model model;
+  Face face1;
+  Face face2;
+  Face face3;
+  Face face4;
+  Face face5;
+  Face face6;
+
   Vector3 position;
   BLOCK_TYPE type;
   bool render = true;
@@ -29,7 +52,15 @@ struct Block
   DrawBlock ()
   {
     if (render)
-      DrawModel (model, position, 1.0f, WHITE);
+    {
+      face1.DrawFace ();
+      face2.DrawFace ();
+      face3.DrawFace ();
+      face4.DrawFace ();
+      face5.DrawFace ();
+      face6.DrawFace ();
+    }
+    // DrawModel (model, position, 1.0f, WHITE);
   }
 };
 
@@ -69,6 +100,103 @@ printAll (const std::vector<Block>& blocks)
   }
   print ("]");
   println ("");
+}
+
+void
+SetAndDetermineRender (std::vector<Block>& blocks)
+{
+  Texture2D tx = LoadTexture ("grass.png");
+  unsigned i = 0;
+  for (Block& b : blocks)
+  {
+
+    b.face1.render = false;
+    b.face2.render = false;
+    b.face3.render = false;
+    b.face4.render = false;
+    b.face5.render = false;
+    b.face6.render = false;
+
+    if (b.type == BLOCK_TYPE::AIR)
+    {
+      b.render = false;
+      continue;
+    }
+    bool drawY = false;
+    Vector3 bPos = b.position;
+    if (getBlock ({ bPos.x, bPos.y + 1, bPos.z }, blocks) == BLOCK_TYPE::AIR)
+    {
+      b.face1.model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
+      b.face1.position = { bPos.x, bPos.y + 0.5f, bPos.z };
+      b.face1.rotation = { 1.0, 0.0, 0.0 };
+      b.face1.angle = 0.0f;
+      b.face1.render = true;
+      b.face1.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+      println ("BLOCK{}[FACE1DONE]", i);
+    }
+
+    if (getBlock ({ bPos.x, bPos.y - 1, bPos.z }, blocks) == BLOCK_TYPE::AIR)
+    {
+      b.face2.model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
+      b.face2.position = { bPos.x, bPos.y - 0.5f, bPos.z };
+      b.face2.rotation = { 1.0, 0.0, 0.0 };
+      b.face2.angle = 180.0f;
+      b.face2.render = true;
+      b.face2.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+      println ("BLOCK{}[FACE2DONE]", i);
+    }
+    // drawY = true;
+
+    bool drawX = false;
+    if (getBlock ({ bPos.x + 1, bPos.y, bPos.z }, blocks) == BLOCK_TYPE::AIR)
+    {
+      b.face3.model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
+      b.face3.position = { bPos.x + 0.5f, bPos.y, bPos.z };
+      b.face3.rotation = { 0.0, 0.0, 1.0 };
+      b.face3.angle = -90.0f;
+      b.face3.render = true;
+      b.face3.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+      println ("BLOCK{}[FACE3DONE]", i);
+    }
+
+    if (getBlock ({ bPos.x - 1, bPos.y, bPos.z }, blocks) == BLOCK_TYPE::AIR)
+    {
+      b.face4.model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
+      b.face4.position = { bPos.x - 0.5f, bPos.y, bPos.z };
+      b.face4.rotation = { 0.0, 0.0, 1.0 };
+      b.face4.angle = 90.0f;
+      b.face4.render = true;
+      b.face4.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+      println ("BLOCK{}[FACE4DONE]", i);
+    }
+
+    bool drawZ = false;
+    if (getBlock ({ bPos.x, bPos.y, bPos.z + 1 }, blocks) == BLOCK_TYPE::AIR)
+    {
+      b.face5.model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
+      b.face5.position = { bPos.x, bPos.y, bPos.z + 0.5f };
+      b.face5.rotation = { 1.0, 0.0, 0.0 };
+      b.face5.angle = 90.0f;
+      b.face5.render = true;
+      b.face5.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+      println ("BLOCK{}[FACE5DONE]", i);
+    }
+
+    if (getBlock ({ bPos.x, bPos.y, bPos.z - 1 }, blocks) == BLOCK_TYPE::AIR)
+    {
+      b.face6.model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
+      b.face6.position = { bPos.x, bPos.y, bPos.z - 0.5f };
+      b.face6.rotation = { 1.0, 0.0, 0.0 };
+      b.face6.angle = -90.0f;
+      b.face6.render = true;
+      b.face6.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+      println ("BLOCK{}[FACE6DONE]", i);
+    }
+    b.render = true;
+    ++i;
+
+    // println ("BLOCK END");
+  }
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "raylib.h"
+
 #include "rcamera.h"
 
 #include "Block.h"
@@ -14,6 +15,7 @@
 int
 main ()
 {
+
   SetConfigFlags (FLAG_WINDOW_HIGHDPI);
 
   InitWindow (1280, 720, "Hello Raylib");
@@ -22,90 +24,67 @@ main ()
   HideCursor ();
   DisableCursor ();
 
-  Texture2D tx = LoadTexture ("grass.png");
-
-  Image noise = GenImagePerlinNoise (16, 16, 0.0, 0.0, 1.0f);
+  Image noise = GenImagePerlinNoise (128, 128, 0.0, 0.0, 1.0f);
   Texture2D noiseTex = LoadTextureFromImage (noise);
 
   std::vector<Block> ents (33296);
 
-  // Color* pixel = LoadImageColors (noise);
-
-  // int height = noise.height;
-  // int width = noise.width;
-
-  // for (int x = 0; x < width; ++x)
+  // for (int x = 0; x < 16; ++x)
   // {
-  //   for (int z = 0; z < height; ++z)
+  //   for (int z = 0; z < 16; ++z)
   //   {
-  //     float normalizedHeight = GetGrayScale (pixel[z * width + x]);
-  //     int yHeight = lround (normalizedHeight * 0.1f);
-  //     println ("Y SIZE {}", yHeight);
-  //     for (int y = 0; y < yHeight; ++y)
+  //     for (int y = 0; y < 32; ++y)
   //     {
+  //       Block en =
+  //         Block { .face1 = NULL,
+  //                 .face2 = NULL,
+  //                 .face3 = NULL,
+  //                 .face4 = NULL,
+  //                 .face5 = NULL,
+  //                 .face6 = NULL,
+  //                 .position = Vector3 { (float) x, (float) y, (float) z },
+  //                 .type = BLOCK_TYPE::GROUND };
 
-  //       Entity en =
-  //         Entity { .model = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1,
-  //         1)),
-  //                  .Position =
-  //                    Vector3 { (float) x * 2, (float) y * 2, (float) z * 2 }
-  //                    };
+  //       // en.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+  //       ents[PointToIndex (en.position)] = en;
 
-  //       en.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
-  //       ents.push_back (en);
+  //       // ents.push_back (en);
   //     }
   //   }
   // }
-  for (int x = 0; x < 16; ++x)
+
+  Color* pixel = LoadImageColors (noise);
+
+  int height = noise.height;
+  int width = noise.width;
+
+  for (int x = 0; x < width; ++x)
   {
-    for (int z = 0; z < 16; ++z)
+    for (int z = 0; z < height; ++z)
     {
-      for (int y = 0; y < 32; ++y)
+      float normalizedHeight = GetGrayScale (pixel[z * width + x]);
+      int yHeight = lround (normalizedHeight * 0.1f);
+      println ("Y SIZE {}", yHeight);
+      for (int y = 0; y < yHeight; ++y)
       {
+
         Block en =
-          Block { .model = LoadModelFromMesh (GenMeshCube (1.0f, 1.0f, 1.0f)),
+          Block { .face1 = NULL,
+                  .face2 = NULL,
+                  .face3 = NULL,
+                  .face4 = NULL,
+                  .face5 = NULL,
+                  .face6 = NULL,
                   .position = Vector3 { (float) x, (float) y, (float) z },
                   .type = BLOCK_TYPE::GROUND };
 
-        en.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
+        // en.model.materials->maps[MATERIAL_MAP_DIFFUSE].texture = tx;
         ents[PointToIndex (en.position)] = en;
-
-        // ents.push_back (en);
       }
     }
   }
 
-  for (Block& b : ents)
-  {
-    if (b.type == BLOCK_TYPE::AIR)
-    {
-      b.render = false;
-      continue;
-    }
-    bool drawY = false;
-    Vector3 bPos = b.position;
-    if (getBlock ({ bPos.x, bPos.y + 1, bPos.z }, ents) == BLOCK_TYPE::GROUND &&
-        getBlock ({ bPos.x, bPos.y - 1, bPos.z }, ents) ==
-          BLOCK_TYPE::GROUND) // handle blocks above and below
-      drawY = true;
-
-    bool drawX = false;
-    if (getBlock ({ bPos.x + 1, bPos.y, bPos.z }, ents) == BLOCK_TYPE::GROUND &&
-        getBlock ({ bPos.x - 1, bPos.y, bPos.z }, ents) ==
-          BLOCK_TYPE::GROUND) // handle blocks above and below
-      drawX = true;
-
-    bool drawZ = false;
-    if (getBlock ({ bPos.x, bPos.y, bPos.z + 1 }, ents) == BLOCK_TYPE::GROUND &&
-        getBlock ({ bPos.x, bPos.y, bPos.z - 1 }, ents) ==
-          BLOCK_TYPE::GROUND) // handle blocks above and below
-      drawZ = true;
-
-    if (drawY && drawZ && drawX)
-      b.render = false;
-
-    // println ("BLOCK END");
-  }
+  SetAndDetermineRender (ents);
 
   // Entity* pln = new Entity { plane, Vector3 { 0.0f, 0.0f, 0.0f } };
 
@@ -116,6 +95,8 @@ main ()
   Player* player = new Player (Vector3 { 10.0f, 2.0f, 10.0f }, world);
 
   // SetTargetFPS (60);
+
+  Model test = LoadModelFromMesh (GenMeshPlane (1.0f, 1.0f, 1, 1));
 
   // game loop
   while (!WindowShouldClose ())
@@ -138,8 +119,48 @@ main ()
           continue;
         e.DrawBlock ();
       }
+      // DrawModelEx (test,
+      //              { 0.0, 0.5, 0.0 },
+      //              { 1.0, 0.0, 0.0 },
+      //              180,
+      //              Vector3One (),
+      //              RED); // negy
 
-      //
+      // DrawModelEx (test,
+      //              { 0.0, 1.5, 0.0 },
+      //              { 1.0, 0.0, 0.0 },
+      //              0,
+      //              Vector3One (),
+      //              WHITE); // posy
+
+      // DrawModelEx (test,
+      //              { 0.0, 1.0, 0.5 },
+      //              { 1.0, 0.0, 0.0 },
+      //              90,
+      //              Vector3One (),
+      //              GREEN); // posz
+
+      // DrawModelEx (test,
+      //              { 0.0, 1.0, -0.5 },
+      //              { 1.0, 0.0, 0.0 },
+      //              -90,
+      //              Vector3One (),
+      //              BLUE); // negz
+
+      // DrawModelEx (test,
+      //              { -0.5, 1.0, 0.0 },
+      //              { 0.0, 0.0, 1.0 },
+      //              90,
+      //              Vector3One (),
+      //              YELLOW); // negx
+
+      // DrawModelEx (test,
+      //              { 0.5, 1.0, 0.0 },
+      //              { 0.0, 0.0, 1.0 },
+      //              -90,
+      //              Vector3One (),
+      //              ORANGE); // posx
+      // //
 
       EndMode3D ();
     }
